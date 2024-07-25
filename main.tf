b17ddb9486cf8a28444a654b35ca6ef049df722a
sfurlani <suelenfurlani@hotmail.com>
2024-07-24 23:21:36 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,6 @@ terraform {
 }
 provider "aws" {
  region     = "us-east-1"
- access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
- secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
 }
  
 resource "aws_s3_bucket" "sfurlani" {
