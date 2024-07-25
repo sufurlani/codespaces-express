cc94ec40af0be39eedc869113fd195a60c831ead
sfurlani <suelenfurlani@hotmail.com>
2024-07-24 23:14:57 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
-terraform {
- required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.59.0"
-    }
-  }
 
+terraform {
   cloud {
     organization = "sfurlani"
 
@@ -13,13 +7,4 @@ terraform {
       name = "codespaces-express"
     }
   }
-}
-
-provider "aws" {
-  region  = "us-east-1"
-}
-
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "devops"
-  acl    = "private"
 }
