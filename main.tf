c255f79e31a528e7b1aa13e863860d3f28c65809
sfurlani <suelenfurlani@hotmail.com>
2024-07-24 23:20:29 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,3 +8,13 @@ terraform {
     }
   }
 }
+provider "aws" {
+ region     = "us-east-1"
+ access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
+ secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+}
+ 
+resource "aws_s3_bucket" "spacelift-test1-s3" {
+   bucket = "spacelift-test1-s3"
+   acl = "private"  
+}
