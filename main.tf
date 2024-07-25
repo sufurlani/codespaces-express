83072feab06554d98bb1fd33c7948135d0cd9804
sfurlani <suelenfurlani@hotmail.com>
2024-07-24 22:50:50 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,10 @@
-# The configuration for the `remote` backend.
-  terraform {
-   backend "remote" {
-     # The name of your Terraform Cloud organization.
-     organization = "sfurlani"
-  
-     # The name of the Terraform Cloud workspace to store Terraform state files in.
-     workspaces {
-       name = "codespaces-express"
-     }
-   }
+
+terraform {
+  cloud {
+    organization = "sfurlani"
+
+    workspaces {
+      name = "codespaces-express"
+    }
   }
-# An example resource that does nothing.
- resource "null_resource" "example" {
-   triggers = {
-     value = "A example resource that does nothing!"
-   }
- }
+}
